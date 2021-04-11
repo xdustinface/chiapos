0f0471fcc8ff6390b2ef46563df57565b788c438
xdustinface <xdustinfacex@gmail.com>
2021-04-11 14:44:16 +0200

    src: Cut of decimals in progress + print a `%`

--- src/progress.hpp
+++ src/progress.hpp
@@ -5,7 +5,7 @@
 
 void progress(int phase, int n, int max_n) {
     float p = (100.0 / 4) * ((phase - 1.0) + (1.0 * n / max_n));
-    Util::Log("Progress: %s\n", p);
+    Util::Log("Progress: %s%%\n", static_cast<int64_t>(p));
 }
 
 #endif  // SRC_CPP_PROGRESS_HPP
