81370af745d61ed17baa615bb9166d4e58727e24
xdustinface <xdustinfacex@gmail.com>
2022-01-12 11:42:06 +0100

    python-bindings|tests: Introduce `DiskProver.{__bytes__|from_bytes}`

--- python-bindings/chiapos.cpp
+++ python-bindings/chiapos.cpp
@@ -77,6 +77,16 @@ PYBIND11_MODULE(chiapos, m)
 
     py::class_<DiskProver>(m, "DiskProver")
         .def(py::init<const std::string &>())
+        .def_static("from_bytes", [](const py::bytes &bytes) -> DiskProver {
+            py::buffer_info info(py::buffer(bytes).request());
+            auto data = reinterpret_cast<const uint8_t*>(info.ptr);
+            auto vecBytes = std::vector<uint8_t>(data, data + info.size);
+            return DiskProver(vecBytes);
+        })
+        .def("__bytes__", [](DiskProver &dp) {
+            std::vector<uint8_t> vecBytes = dp.ToBytes();
+            return py::bytes(reinterpret_cast<const char*>(vecBytes.data()), vecBytes.size());
+        })
         .def(
             "get_memo",
             [](DiskProver &dp) {
